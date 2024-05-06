cc90ff3b558ec0aefcfd70a2a0b8634faa5f82d8
uncleaar <danilyudin723@gmaill.com>
2024-05-06 17:18:52 +0500

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import {Button} from "@/shared/ui/button";
+
+export default function Home() {
+  return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <Button>button</Button>
+      </main>
+  );
+}
